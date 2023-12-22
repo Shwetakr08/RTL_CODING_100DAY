228ba0a002e4df4bcc772cddaed8ac96e5895976
Shwetakr08 <135629209+Shwetakr08@users.noreply.github.com>
2023-12-22 20:48:50 +0530

    Create one_hot_tb.v

---
+++ Binary_to_one_hot_encoder/one_hot_tb.v
@@ -0,0 +1,31 @@
+`include "one_hot.v"
+module one_hot_tb();
+
+  localparam BIN_W = 5;
+  localparam ONE_HOT_W = 32;
+  integer i;
+  
+
+  reg [BIN_W-1:0] bin_i;
+  wire [ONE_HOT_W-1:0] one_hot_o;
+
+  one_hot dut(bin_i,one_hot_o);
+
+  initial 
+  begin
+    for( i=0; i<32; i=i+1)
+	begin
+      bin_i = i;
+      #5;
+    end
+	
+  
+  end
+  
+  initial
+  begin
+  $monitor("Binary Value: %b | One hot encoded value : %b", bin_i,one_hot_o);
+  #300 $finish;
+  end
+
+endmodule
